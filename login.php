456883ce612ca0a59eb976bb15b59868f98e7643
Anchit Kolla <Anchit Kolla>
2016-11-28 23:00:01 -0500

    weird syntax error

--- login.php
+++ login.php
@@ -179,7 +179,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
     <!-- Placed at the end of the document so the pages load faster -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script></script>
     <script src="dist/js/bootstrap.min.js"></script>
 
     <!--jQuery Form Validator-->
