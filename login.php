5a873e13346467967f8d0b195a9cce5e9f0f12b0
Ben Ottman <Ben Ottman>
2016-11-28 21:14:54 -0500

    Working search function. Currently has sample table

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 
 <!DOCTYPE html>
-
 <html lang="en">
 <?php
 if(isset($_COOKIE["logged_in"])) {
--- search.php
+++ search.php
@@ -1,7 +1,8 @@
-
-﻿
 <!DOCTYPE html>
 <html lang="en">
+<?php
+session_start();
+?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,20 +10,19 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-
-    <title>YardSale</title>
+    <title>Yardsale</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="./assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
-
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="./assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -31,9 +31,14 @@
     <![endif]-->
 
     <!-- Custom styles for this template -->
-
     <link href="carousel.css" rel="stylesheet">
-
+    <style>
+    .middlealign {
+       display: inline-block;
+    vertical-align: middle;
+    float: none;
+}
+    </style>
   </head>
 <!-- NAVBAR
 ================================================== -->
@@ -56,11 +61,32 @@
               <ul class="nav navbar-nav">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
-                <li class="active"><a href="search.php">View Listings</a></li>
-                <li><a href="list.php">Post a Listing</a></li>
-                <li><a href="logout.php">Sign out</a></li>
-
-
+                <?php
+                  if(isset($_COOKIE["logged_in"])) {
+                    echo "<li class='active'><a href='search.php'>View Listings</a></li>";
+                  }
+                ?>
+                <?php
+                  if(isset($_COOKIE["logged_in"])) {
+                    echo "<li><a href='list.php'>Post a Listing</a></li>";
+                  }
+                ?>
+                <?php
+                  if(!isset($_COOKIE["logged_in"])) {
+                    echo "<li><a href='signup.php'>Sign Up</a></li>";
+                  }
+                ?>
+                
+                <?php
+                  if(!isset($_COOKIE["logged_in"])) {
+                    echo "<li><a href='login.php'>Sign in</a></li>";
+                  }
+                ?>
+                <?php
+                  if(isset($_COOKIE["logged_in"])) {
+                    echo "<li><a href='logout.php'>Sign out</a></li>";
+                  }
+                ?>
                 <p class="navbar-text" style="font-style:italic">“Do you prefer to use meters or yards?”</p>
               </ul>
             </div>
@@ -70,12 +96,49 @@
       </div>
     </div>
 
-    <?php
+
+   
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+       <?php
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "yardsale";
-  $sql="SELECT * FROM listings";
+  $location = $_SESSION['location'];
+  $sql="SELECT * FROM listings WHERE city='$location'";
   $conn = new mysqli($servername, $username, $password, $dbname);
   $result=$conn->query($sql);
   while ($row = mysqli_fetch_array($result)) {
@@ -83,13 +146,10 @@
   }
   
 ?>
-
       <!-- FOOTER -->
       <footer>
         <p class="pull-right"><a href="#">Back to top</a></p>
-
-        <p>&copy; 2016 YardSale &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-
+        <p>&copy; 2016 Yardsale &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
 
     </div><!-- /.container -->
@@ -99,13 +159,11 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="dist/js/bootstrap.min.js"></script>
+    <script>window.jQuery || document.write('<script src="./assets/js/vendor/jquery.min.js"></script>')</script>
+    <script src="./dist/js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="assets/js/vendor/holder.min.js"></script>
+    <script src="./assets/js/vendor/holder.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="./assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
-
 </html>
